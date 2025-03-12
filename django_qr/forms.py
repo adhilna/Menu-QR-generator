3cdd755d68357bba187937926edb11beeb494734
adhilna <adhilchy11@gmail.com>
2025-03-12 14:13:25 +0530

    Added generate_qr_code.html and updated generate_qr_code view

--- django_qr/forms.py
+++ django_qr/forms.py
@@ -1,5 +1,19 @@
 from django import forms
 
 class QRcodeForm(forms.Form):
-    restaurant_name = forms.CharField(max_length=128, label='Restaurant Name')
-    url = forms.URLField(max_length=128, label='Menu URL')
+    restaurant_name = forms.CharField(
+        max_length=128,
+        label='Restaurant Name',
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Enter Restaurant Name',
+
+        })
+        )
+    url = forms.URLField(max_length=128,
+                         label='Menu URL',
+                         widget=forms.URLInput(attrs={
+                            'class': 'form-control',
+                            'placeholder': 'Enter the URL for your online-menu'
+                         })
+                         )
--- django_qr/views.py
+++ django_qr/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
-
+from .forms import QRcodeForm
 
 
 def generate_qr_code(request):
-    return render(request, 'generate_qr_code.html')
+    if request.method == 'POST':
+        form = QRcodeForm(request.POST)
+        if form.is_valid():
+            res_name = form.cleaned_data['restaurant_name']
+            url = form.cleaned_data['url']
+    else:
+        form = QRcodeForm()
+        context = {
+            'form' : form
+        }
+        return render(request, 'generate_qr_code.html', context)
