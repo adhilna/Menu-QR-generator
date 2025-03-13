bd20a4988407749e7b6da77c79adf3e0cbb57bce
adhilna <adhilchy11@gmail.com>
2025-03-13 10:13:02 +0530

    Updated QR code generation and fixed styling issues

--- django_qr/views.py
+++ django_qr/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .forms import QRcodeForm
 import qrcode
+from django.conf import settings
+import os
 
 
 def generate_qr_code(request):
@@ -14,7 +16,19 @@ def generate_qr_code(request):
             # GENERATE QR CODE
             qr = qrcode.make(url)
             file_name = res_name.replace(' ', '_').lower() + '_menu.png'
-            qr.save(file_name)
+            file_path = os.path.join(settings.MEDIA_ROOT, file_name)   # ../media/the_kitchen_menu.png
+            qr.save(file_path)
+
+
+            # CREATE IMAGE URL
+            qr_url = settings.MEDIA_URL + file_name  # This should be correct
+
+
+            context = {
+                'res_name': res_name,
+                'qr_url': qr_url
+            }
+            return render(request, 'rq_result.html', context)
 
     else:
         form = QRcodeForm()
