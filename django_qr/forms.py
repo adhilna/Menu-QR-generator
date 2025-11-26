9aa84c599b3e0ae9ff2cb3d22e05bc0a238cecc9
adhilna <adhilchy11@gmail.com>
2025-11-26 10:14:10 +0530

    some text changes

--- django_qr/forms.py
+++ django_qr/forms.py
@@ -3,18 +3,18 @@ from django import forms
 class QRcodeForm(forms.Form):
     restaurant_name = forms.CharField(
         max_length=128,
-        label='Restaurant Name',
+        label='Name',
         widget=forms.TextInput(attrs={
             'class': 'form-control',
-            'placeholder': 'Enter Restaurant Name',
+            'placeholder': 'Enter name for your QR',
 
         })
         )
     url = forms.URLField(
         max_length=128,
-        label='Menu URL',
+        label='URL',
         widget=forms.URLInput(attrs={
             'class': 'form-control',
-            'placeholder': 'Enter the URL for your online-menu'
+            'placeholder': 'Enter the URL'
         })
         )
